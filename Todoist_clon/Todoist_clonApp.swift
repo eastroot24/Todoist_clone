f582878d766ef0d7b80ac30902f26ca5be1df7f4
eastroot24 <eastroot24@gmail.com>
2025-02-15 19:33:41 +0900

    Initial Commit

---
+++ Todoist_clon/Todoist_clonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todoist_clonApp.swift
+//  Todoist_clon
+//
+//  Created by eastroot on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Todoist_clonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Todoist_clonTests/Todoist_clonTests.swift
@@ -0,0 +1,17 @@
+//
+//  Todoist_clonTests.swift
+//  Todoist_clonTests
+//
+//  Created by eastroot on 2/15/25.
+//
+
+import Testing
+@testable import Todoist_clon
+
+struct Todoist_clonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
